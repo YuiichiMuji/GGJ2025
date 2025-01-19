b462143e349b21c132767973eaeba2883a6a0c53
YuiichiMuji <yuiichimuji@gmail.com>
2025-01-19 12:29:20 +0800

    awd

--- Assets/LevelSelect.cs
+++ Assets/LevelSelect.cs
@@ -7,12 +7,12 @@ public class LevelSelect : MonoBehaviour
 {
     public void startlvl1()
     {
-        SceneManager.LoadScene("LevelOne");
-    }
-    public void startlvl2() {
         SceneManager.LoadScene("LevelTwo");
     }
-    public void startlvl3() {
+    public void startlvl2() {
         SceneManager.LoadScene("LevelThree");
+    }
+    public void startlvl3() {
+        SceneManager.LoadScene("LevelFour");
     }
 }
