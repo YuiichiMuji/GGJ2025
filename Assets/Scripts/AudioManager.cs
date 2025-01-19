60d7d0561ef5078e52392938055095ed9c9cd5a5
Gyb <479276132@qq.com>
2025-01-19 11:42:27 +0800

    bgm

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager Instance;
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
